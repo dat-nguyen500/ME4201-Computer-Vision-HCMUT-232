ab71ecee74d17fb5b8dd1521187911751f73b7f8
dat-nguyen500 <151296540+dat-nguyen500@users.noreply.github.com>
2024-01-20 18:23:07 +0700

    Create ex_2.7.cpp

---
+++ Textbook/Chapter 2/ex_2.7.cpp
@@ -0,0 +1,9 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void histogram(string const& name, Mat const& Image)
