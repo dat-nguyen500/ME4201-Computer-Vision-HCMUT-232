abd6f8abb7e0098afa7cd7165e68d1accd6871e0
dat-nguyen500 <151296540+dat-nguyen500@users.noreply.github.com>
2024-02-21 07:26:18 +0700

    Create Exercise 4a.cpp

---
+++ Assigment/Assignment 1/Exercise 4a.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    float sigma = 0.3;
+    Mat src = (Mat_<float>(5, 7) << 9, 10, 11, 10, 9, 10, 11,
+        10, 9, 95, 95, 95, 10, 11,
+        10, 95, 10, 11, 10, 95, 11,
+        10, 9, 95, 95, 95, 13, 11,
+        10, 10, 10, 10, 10, 13, 1);
+    Mat dst;
+    GaussianBlur(src, dst, Size(3, 3), sigma, sigma, BORDER_REPLICATE);
+    cout << "Image Matrix" << endl << "" << src << endl;
+    cout << "Gaussian Blur" << endl << "" << dst << endl;
+    waitKey();
+}
